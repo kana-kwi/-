206d1b62ff68b84434570beb1b20dad01c248065
有马加奈 <ymjnkawa1@gmail.com>
2025-08-08 03:39:56 +0800

    Update config.js

--- themes/photo/config.js
+++ themes/photo/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   PHOTO_MENU_SEARCH: true, // 显示搜索
   PHOTO_HOME_BACKGROUND: false, // 首页是否显示背景图, 默认关闭
 
-  PHOTO_ARTICLE_RECOMMEND: true, // 推荐关联内容在文章底部
+  PHOTO_ARTICLE_RECOMMEND: false, // 推荐关联内容在文章底部
   PHOTO_VIDEO_COMBINE: true, // 聚合视频，开启后一篇文章内的多个含caption的视频会被合并到文章开头，并展示分集按钮
   PHOTO_VIDEO_COMBINE_SHOW_PAGE_FORCE: false, // 即使只有一集也显示集数切换按钮
   PHOTO_PREVIEW_CATEGORY_COUNT: 16, // 首页最多展示的分类数量，0为不限制
